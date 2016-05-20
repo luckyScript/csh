8be14d92a22b3370aec8639ac43c01a2cd0a4047
luckyscript <main.lukai@gmail.com>
2016-05-20 13:52:38 +0800

    touch

--- assignment1.c
+++ assignment1.c
@@ -1,8 +1,15 @@
+/**
+* luckyscript
+* a shell REPL
+* cd/mkdir/touch/cat/ls/exit
+*/
 #include <stdio.h>
 #include <string.h>
 #include <dirent.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 
 void ls(char dirname[100], char args[10]);
@@ -38,7 +45,7 @@ int main(int argc,char **argv) {
         
         while(1) {
             char args[20];
-            if(line &&(sscanf(++line, "%20s", args) == 1)) {
+            if (line &&(sscanf(++line, "%20s", args) == 1)) {
                 arguments[argscount] = malloc(sizeof(char) * 20);
                 strncpy(arguments[argscount], args, 20);
                 argscount++;
@@ -50,26 +57,28 @@ int main(int argc,char **argv) {
         }
         //parser end
         
-        if(strcmp(command, "ls") == 0) {
-            if(argscount == 0) {
+        if (strcmp(command, "ls") == 0) {
+            if (argscount == 0) {
                 ls(cwd, "ls");
-            } else if(argscount == 1 && (strcmp(arguments[0], "-l") == 0)) {
+            } else if (argscount == 1 && (strcmp(arguments[0], "-l") == 0)) {
                 ls(cwd, "ll");
-            }else {
+            }else if (argscount == 1) {
                 ls(cwd, arguments[0]);
+            } else {
+                printf("warning: too many argyments\n");
             }
         
-        } else if(strcmp(command, "echo") == 0) {
-            if(argscount == 1 && strcmp(arguments[0], "$HOME") == 0) {
+        } else if (strcmp(command, "echo") == 0) {
+            if (argscount == 1 && strcmp(arguments[0], "$HOME") == 0) {
                 printf("%s\n", getenv("HOME"));
             }else {
                 echo(arguments, argscount);
             }
         
-        } else if(strcmp(command, "cd") == 0) {
-            if(argscount == 2) {
+        } else if (strcmp(command, "cd") == 0) {
+            if (argscount == 2) {
                 printf("warning: too many arguments\n");
-            } else if(argscount == 0 || (strcmp(arguments[0], ".") == 0)) {
+            } else if (argscount == 0 || (strcmp(arguments[0], ".") == 0)) {
                 // do nothing
             } else {
                 int rs = chdir(arguments[0]);
@@ -77,6 +86,39 @@ int main(int argc,char **argv) {
                 sprintf(shell,"[%s]@myshell:%s>",user,cwd);
             }
         
+        } else if (strcmp(command, "mkdir") == 0) {
+            if (argscount == 1) {
+                mkdir(arguments[0],777);
+            } else {
+                printf("warning: too many or few arguments\n");
+            }
+            
+        } else if (strcmp(command, "cat") == 0) {
+            if (argscount == 1) {
+                FILE *fp = NULL;
+                char c;
+                fp = fopen(arguments[0], "r");
+                if (fp == NULL) {
+                    printf("file not exits or other error");
+                }
+                while(fscanf(fp, "%c", &c) != EOF) {
+                    printf("%c", c);
+                }
+                fclose(fp);
+                fp = NULL;
+                printf("\n");
+            } else {
+                printf("warning: too many or few arguments\n");
+            }
+        } else if (strcmp(command, "touch") == 0) {
+            if (argscount == 1) {
+                FILE *fp = NULL;
+                fp = fopen(arguments[0], "a+");
+                fclose(fp);
+                fp = NULL;
+            } else {
+                printf("warning: too many or few arguments\n");
+            }
         } else {
             printf("command not found!");
         }
@@ -98,14 +140,14 @@ void ls(char dirname[100], char args[10]) {
     DIR *dp;
     struct dirent *dirp;
     // char dirname[20] = "./";
-    if(strcmp(args,"ls") == 0 || strcmp(args,"ll") == 0) {
+    if (strcmp(args,"ls") == 0 || strcmp(args,"ll") == 0) {
         dp = opendir(dirname);
         while((dirp = readdir(dp)) != NULL) {
-            if(strcmp(args,"ls") == 0) {
-                if(dirp->d_name[0] != '.') {
+            if (strcmp(args,"ls") == 0) {
+                if (dirp->d_name[0] != '.') {
                     printf("%s ", dirp->d_name);
                 }
-            } else if(strcmp(args,"ll") == 0) {
+            } else if (strcmp(args,"ll") == 0) {
                 printf("%s ", dirp->d_name);
             }
         }
--- test.c
+++
@@ -1,9 +0,0 @@
-#include "stdio.h"
-#include "string.h"
-int
-main(void) {
-	char dirname[20] = "./";
-	char args[10] = ".git";
-	strcat(dirname, args);
-	printf("%s", dirname);
-}
